0732489e3ea9b0fa4278a4c70991ff050359ba07
NCTona <143495151+NCTona@users.noreply.github.com>
2024-06-14 17:39:45 +0700

    validator

--- validator.js
+++ validator.js
@@ -10,11 +10,12 @@ function Validator(options) {
             var inputElement = formElement.querySelector(rule.selector);
             var errorElement = inputElement.parentElement.querySelector('.alert');
             var input = inputElement.parentElement.querySelector('.form-input');
-
+            var button = document.querySelector('.form-button');
+            
             if (inputElement) {
                 inputElement.onblur = function () {
                     var errorMessage = rule.test(inputElement.value);
-
+                    
                     if (errorMessage) {
                         errorElement.innerText = errorMessage;
                         input.setAttribute("style", "border: 2px solid rgb(195, 89, 89);")
@@ -26,6 +27,17 @@ function Validator(options) {
                 inputElement.oninput = function () {
                     errorElement.innerText = " ";
                     input.setAttribute("style", "border: 2px solid #b3ff3a;")
+                }
+                button.onclick = function () {
+                    var errorMessage = rule.test(inputElement.value);
+
+                    if (errorMessage) {
+                        errorElement.innerText = errorMessage;
+                        input.setAttribute("style", "border: 2px solid rgb(195, 89, 89);")
+                    } else {
+                        errorElement.innerText = " ";
+                        input.setAttribute("style", "border: 2px solid #fffd8d;")
+                    }
                 }
             }
         })
@@ -53,14 +65,5 @@ Validator.isPassword = function (selector) {
         }
     }
 }
-
-Validator({
-    form: "#form",
-    rules: [
-        Validator.isUsername("#username"),
-        Validator.isPassword("#password"),
-    ],
-});
-
 
 
